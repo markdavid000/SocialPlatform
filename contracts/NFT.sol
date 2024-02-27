a8321fa7590f54a25b5b1b3e4d236b3c33ef871a
markdavid000 <davidojukwu59@gmail.com>
2024-02-27 21:52:56 +0100

    contract in progress

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFT is ERC721URIStorage {
-    uint256 private tokenIdCounter;
-    constructor(string memory _tokenName, string memory _tokenSymbol) ERC721(_tokenName, _tokenSymbol){}
+// contract NFT is ERC721URIStorage {
+//     uint256 private tokenIdCounter;
+//     constructor(string memory _tokenName, string memory _tokenSymbol) ERC721(_tokenName, _tokenSymbol){}
 
-    function createNFT(string memory _tokenURI) external {
-        uint256 tokenId = tokenIdCounter++;
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, _tokenURI);
-    }
+//     function createNFT(string memory _tokenURI) external {
+//         uint256 tokenId = tokenIdCounter++;
+//         _safeMint(msg.sender, tokenId);
+//         _setTokenURI(tokenId, _tokenURI);
+//     }
 
-    function getTokenURI(uint256 _tokenId) external view returns(string memory) {
-        return tokenURI(_tokenId);
-    }
-}
+//     function getTokenURI(uint256 _tokenId) external view returns(string memory) {
+//         return tokenURI(_tokenId);
+//     }
+// }
 
